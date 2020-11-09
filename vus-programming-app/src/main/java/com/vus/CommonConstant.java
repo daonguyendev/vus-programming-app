ce2affdb74420476aeec9ccd577d9c8b8085cc22
Dao Nguyen <daonguyen.dev@gmail.com>
2020-11-09 13:03:56 +0700

    Technical Sharing Session about technology in our lives

---
+++ vus-programming-app/src/main/java/com/vus/CommonConstant.java
@@ -0,0 +1,32 @@
+package com.vus;
+
+public class CommonConstant {
+
+	// Email Sender Constants
+    public static final int SSL_PORT = 465;
+    public static final int TSL_PORT = 587;
+	public static final boolean IS_AUTH_ENABLE = true;
+    public static final String SMTP_AUTH = "mail.smtp.auth";
+    public static final String SMTP_HOST = "mail.smtp.host";
+    public static final String SMTP_SOCKET_FACTORY_PORT = "mail.smtp.socketFactory.port";
+    public static final String SMTP_SOCKET_FACTORY_CLASS = "mail.smtp.socketFactory.class";
+    public static final String SMTP_PORT = "mail.smtp.port";
+	public static final String HOST_NAME = "smtp.gmail.com";
+    public static final String SSL_CLASS = "javax.net.ssl.SSLSocketFactory";
+    public static final String APP_EMAIL = "vus.programming@gmail.com";
+    public static final String APP_PASSWORD = "Vus@PgMJS2020";
+    public static final String FIRST_RECEIVE_EMAIL = "daonguyen.dev@gmail.com";
+    public static final String SECOND_RECEIVE_EMAIL = "lephung207@gmail.com";
+    public static final String EMAIL_SUBJECT = "VUS Technical Sharing";
+    public static final String EMAIL_CONTENT = "Xin chào mọi người. Chào mừng mọi người đến với buổi "
+    		+ "chia sẻ của mình về ứng dụng của công nghệ trong đời sống của chúng ta ^^";
+    public static final String EMAIL_SENDING_CONFIRMATION = "Thư của bạn đã được gửi thành công!!!";
+    
+    // Page Poster Constants
+    public static final String ACCESS_TOKEN = "EAAFC0U3klsQBAJNycntWnX2P9wbS7e5Epo8cF7gYCtZBkR4kRCmurydZAGCDjxzwKDAuptTZAPDJHcnxx7EjBb5AdtRylg26tfPMF2Si5nXJRFZCXCORuRcpcLbit0sNQyuKZBlUMpFZApw1TORisFe8VJj2E2g3Bu9vuS91AoFCIQNwOYTavs0N2naBnrEOtr8oz230PTHgZDZD";
+    public static final String PAGE_ID = "106332297955711";
+    public static final String ANSWER_YES = "Yes";
+    public static final String MESSAGE_PARAMETER = "message";
+    public static final String PAGE_NEW_FEED_URL = "/feed";
+    public static final String FACEBOOK_URL = "fb.com/";
+}
