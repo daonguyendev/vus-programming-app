f74d22675262b69bd5fe5cdd4115fb290de11d23
daonguyen <daonguyen.dev@gmail.com>
2020-11-17 14:57:07 +0700

    Add lesson 06 exercise

--- vus-programming-app/src/main/java/com/vus/CommonConstant.java
+++ vus-programming-app/src/main/java/com/vus/CommonConstant.java
@@ -16,14 +16,15 @@ public class CommonConstant {
     public static final String APP_EMAIL = "vus.programming@gmail.com";
     public static final String APP_PASSWORD = "Vus@PgMJS2020";
     public static final String FIRST_RECEIVE_EMAIL = "daonguyen.dev@gmail.com";
-    public static final String SECOND_RECEIVE_EMAIL = "lephung207@gmail.com";
+    public static final String SECOND_RECEIVE_EMAIL = "kiettrinh.phung@gmail.com";
     public static final String EMAIL_SUBJECT = "VUS Technical Sharing";
-    public static final String EMAIL_CONTENT = "Xin chào mọi người. Chào mừng mọi người đến với buổi "
-    		+ "chia sẻ của mình về ứng dụng của công nghệ trong đời sống của chúng ta ^^";
+    public static final String EMAIL_CONTENT = "Lô, mail qua chưa vẫy :v??";
+//    public static final String EMAIL_CONTENT = "Xin chào mọi người. Chào mừng mọi người đến với buổi "
+//    		+ "chia sẻ của mình về ứng dụng của công nghệ trong đời sống của chúng ta ^^";
     public static final String EMAIL_SENDING_CONFIRMATION = "Thư của bạn đã được gửi thành công!!!";
     
     // Page Poster Constants
-    public static final String ACCESS_TOKEN = "EAAFC0U3klsQBAJNycntWnX2P9wbS7e5Epo8cF7gYCtZBkR4kRCmurydZAGCDjxzwKDAuptTZAPDJHcnxx7EjBb5AdtRylg26tfPMF2Si5nXJRFZCXCORuRcpcLbit0sNQyuKZBlUMpFZApw1TORisFe8VJj2E2g3Bu9vuS91AoFCIQNwOYTavs0N2naBnrEOtr8oz230PTHgZDZD";
+    public static final String ACCESS_TOKEN = "EAAFC0U3klsQBAGjy0yR5BfjR97VHWCrBFBcWS9SAMjMGUVk7SZCXAAFZAJqgCeSbAwSnVcrAdo9nhGuvtOqHe9ZCRdLoJ86Yrqd6tmym5AIoiGT8VFPxZBxMNgB4l4ue4cNnt7YnVrKZAXYoZABFAgOCtHyYA6N28iBhflvsXegWuZCNzrZAZAZBXi4y4hdi9Y4moZD";
     public static final String PAGE_ID = "106332297955711";
     public static final String ANSWER_YES = "Yes";
     public static final String MESSAGE_PARAMETER = "message";
